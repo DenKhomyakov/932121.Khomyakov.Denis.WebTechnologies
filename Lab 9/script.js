c50667b1571664cb268b2f1c64edcbbe5a59acfd
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2024-09-08 12:01:57 +0700

    Completed the ninth laboratory work

---
+++ Lab 9/script.js
@@ -0,0 +1,62 @@
+let equal_pressed = false;
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll("button[class *='_']").forEach(element =>{
+        element.addEventListener('click',function(element){
+            if(equal_pressed)
+            {
+                document.querySelector('input').value = "";
+                document.querySelector('.input-gray').textContent = "";
+                equal_pressed = false;
+            }
+            if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == true && document.querySelector('input').value.slice(-1) != '.')
+            {
+                document.querySelector('.input-gray').textContent = "";
+            }
+            document.querySelector('input').value+=element.target.textContent;
+            document.querySelector('.input-gray').textContent+=element.target.textContent;
+        })
+    });
+
+    document.querySelector('.dot').addEventListener('click', function(){
+        if(document.querySelector('input').value.slice(-1) != '.')
+        {
+            document.querySelector('input').value+= '.';
+            document.querySelector('.input-gray').textContent+='.'
+        }
+    })
+
+    document.querySelector('.delete').addEventListener('click',function(){
+        document.querySelector('input').value = "";
+        document.querySelector('.input-gray').textContent = "";
+    })
+
+    document.querySelector('.left').addEventListener('click', function(){
+        document.querySelector('input').value = document.querySelector('input').value.slice(0,-1);
+        document.querySelector('.input-gray').textContent=document.querySelector('.input-gray').textContent.slice(0,-1);
+    })
+
+    document.querySelectorAll('.operation').forEach(element =>{
+        element.addEventListener('click', function(element){
+            if (equal_pressed){equal_pressed = false;}
+            if(document.querySelector('input').value.length!=0 ){
+                if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == 1)
+                {
+                    document.querySelector('input').value = document.querySelector('input').value.slice(0,-1);
+                    document.querySelector('.input-gray').textContent =document.querySelector('.input-gray').textContent.slice(0,-1);
+                }
+                document.querySelector('.input-gray').textContent+=element.target.textContent;
+                document.querySelector('input').value+=element.target.textContent;
+            }
+        });
+    })
+
+    document.querySelector('.equals').addEventListener('click',function(){
+        if(isNaN(parseFloat(document.querySelector('input').value.slice(-1))) == 0)
+        {
+            document.querySelector('input').value = eval(document.querySelector('input').value);
+            document.querySelector('.input-gray').textContent = document.querySelector('input').value;
+            equal_pressed = true
+        }
+
+    })
+})
